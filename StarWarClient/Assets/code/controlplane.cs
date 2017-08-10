3e302ae324fc72a1b66e464be45b6dc020534b38
zehuigao <allzysyz@163.com>
2017-08-10 14:32:31 +0800

    [上传新资源]

---
+++ StarWarClient/Assets/code/controlplane.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class controlplane : MonoBehaviour 
+{
+    // Use this for initialization
+    void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+       
+	}
+}
+
+
+public class spaceship : MonoBehaviour
+{
+	//data
+	public float HP;
+	public float DPS;
+	public float speed;
+	public bool is_alive;
+	public coordinate ship_Position_coordinate;
+	public coordinate ship_Rotation_coordinate;
+	public coordinate now_star_coordinate;
+	public coordinate next_star_coordinate;
+	public GameObject shipmodel; 
+	public float ship_around_radius;
+
+    //function
+    public spaceship()
+    {
+        ;
+    }
+
+    public spaceship(GameObject tempship, float thp, float tdps, float tspeed, coordinate now_s_coordinate)  //construction function
+    {
+        shipmodel = tempship;
+        HP = thp;
+        DPS = tdps;
+        speed = tspeed;
+        is_alive = true;
+        now_star_coordinate = now_s_coordinate;
+        next_star_coordinate = now_star_coordinate;
+        ship_around_radius = UnityEngine.Random.Range(1.0f, 2.0f);
+        ship_Position_coordinate = new coordinate(now_star_coordinate.x , now_star_coordinate.y, now_star_coordinate.z + ship_around_radius);
+        ship_Rotation_coordinate = new coordinate(0.0f,0.0f,90.0f);
+        Vector3 ship_position = new Vector3(ship_Position_coordinate.x, ship_Position_coordinate.y, ship_Position_coordinate.z);
+        Quaternion ship_Rotation = Quaternion.Euler(ship_Rotation_coordinate.x, ship_Rotation_coordinate.y, ship_Rotation_coordinate.z);
+        Instantiate(shipmodel, ship_position, ship_Rotation);
+    }
+
+    public void update_ship_transform(float pos_x)    
+    {
+
+        ship_Position_coordinate.z = 
+    }
+
+    public void move_ship()          //let ship move around the star
+    {
+        ship_Position_coordinate.x += speed;
+        update_ship_transform(ship_Position_coordinate.x);
+    }
+
+	/*public void update_ship_transform(float pos_x)
+	{
+		ship_Position_coordinate.x = pos_x;
+		ship_Position_coordinate.y = 0;
+		ship_Position_coordinate.z = (float)Math.Sqrt (ship_around_radius*ship_around_radius - (ship_Position_coordinate.x - now_star_coordinate.x) * (ship_Position_coordinate.x - now_star_coordinate.x))+now_star_coordinate.z;
+		Vector3 ship_position = new Vector3 (ship_Position_coordinate.x, ship_Position_coordinate.y, ship_Position_coordinate.z);
+		ship_Rotation_coordinate.x = 0.0f;
+		ship_Rotation_coordinate.y = (float)(1 / Math.PI) * 180 * (float)Math.Atan ((ship_Position_coordinate.x - now_star_coordinate.x) / (ship_Position_coordinate.z - now_star_coordinate.z));
+		ship_Rotation_coordinate.z = -90.0f;	
+		Quaternion ship_Rotation = Quaternion.Euler(ship_Rotation_coordinate.x, ship_Rotation_coordinate.y, ship_Rotation_coordinate.z); 
+		ship_Position_coordinate.set_coordinate (ship_Position_coordinate.x,ship_Position_coordinate.y,ship_Position_coordinate.z);
+		ship_Rotation_coordinate.set_coordinate (ship_Rotation_coordinate.x,ship_Rotation_coordinate.y,ship_Rotation_coordinate.z);
+	}
+	//function
+	
+	public void move_around_star()
+	{
+		float star_time =  Time.timeSinceLevelLoad;
+		float now_tiem = Time.timeSinceLevelLoad;
+		while (true) 
+		{
+			if (now_tiem - star_time >= 0.03f)
+			{
+				float w = ship_around_radius / speed;
+
+			}
+			
+		}
+	}
+	public void move_spaceship()
+	{
+
+		;
+	}*/
+
+
+}
---
+++ StarWarClient/Assets/code/controlstar.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class controlstar : MonoBehaviour {
+    public GameObject tempshipmodel;
+    // Use this for initialization
+    coordinate star_position = new coordinate();
+    star newstar = new star();
+    void Start () {
+        
+        float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+        float r = transform.localScale.x;
+        star_position.set_coordinate(x, y, z);
+        newstar.set_star(tempshipmodel, 1, star_position, r, 0.1f, 1);
+        newstar.produce_ship();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newstar.produce_ship();
+    }
+
+}
+
+public class coordinate
+{
+	public float x,y,z;
+    public coordinate()
+    {
+        ;
+    }
+	public void set_coordinate(float tempx, float tempy, float tempz)
+	{
+		x = tempx;
+		y = tempy;
+		z = tempz;
+	}
+	public coordinate(float tempx, float tempy, float tempz)
+	{
+		x = tempx;
+		y = tempy;
+		z = tempz;
+	}
+	public coordinate(coordinate temp)
+	{
+		x = temp.x;
+		y = temp.y;
+		z = temp.z;
+	}
+
+};
+
+public class star
+{
+	public int no_star;    //no. of a star
+	public coordinate star_coordinate;   // 坐标
+	public float mass;   // [100~1000]  质量
+	public float radius; // [1~5]  半径
+	public float density; // [1~10]   密度
+	public int belong_who;    //属于哪个玩家
+	public int max_number_berth_ship;    //最大飞船停靠数
+	public float produce_ship_per_second;   //每秒产生飞船数
+	public int max_number_build_battery;    //最大建造防御塔数量
+	public int number_ship_have;     //该星球已拥有星球数量
+    public GameObject ship_mode;     //飞船的预制模型
+
+    public star()
+    {
+        ;
+    }
+	public star(GameObject tshipmode, int no, coordinate star_position, float tempradius, float tempdensiy, int tempbelong)
+	{
+        ship_mode = tshipmode;
+		no_star = no;
+		star_coordinate = new coordinate (star_position);
+		mass = tempdensiy * 0.25f *(float) Math.Pow(tempradius,3)*(float)Math.PI;
+		radius = tempradius;
+		density = tempdensiy;
+		belong_who = tempbelong;
+		max_number_berth_ship = (int)radius * 4;
+		produce_ship_per_second = mass/1000;
+		number_ship_have = 20;
+	}
+    public void set_star(GameObject tshipmode, int no, coordinate star_position, float tempradius, float tempdensiy, int tempbelong)
+    {
+        ship_mode = tshipmode;
+        no_star = no;
+        star_coordinate = new coordinate(star_position);
+        mass = tempdensiy * 0.25f * (float)Math.Pow(tempradius, 3) * (float)Math.PI;
+        radius = tempradius;
+        density = tempdensiy;
+        belong_who = tempbelong;
+        max_number_berth_ship = (int)radius * 4;
+        produce_ship_per_second = mass / 1000;
+        number_ship_have = 20;
+        
+    }
+	public int produce_ship()     
+	{
+        //call ship construction function
+        // send this star's position
+        int num = 1;
+        spaceship ship = new spaceship(ship_mode, 0.1f, 0.1f, 0.1f, star_coordinate);
+        number_ship_have++;
+        return number_ship_have;
+	}
+
+	public int send_ship(int num_send, string no_star)       //get which star we want send ship and how many ship we want send
+	{
+		number_ship_have -= num_send;
+		for (int i = 0; i < num_send; i++) 
+		{
+			//send coordinate of star to this ship;
+		}
+		return max_number_berth_ship - number_ship_have ; // return how many ship we should produce
+	}
+
+	public void adopt_ship ()
+	{
+		;
+	}
+} 
